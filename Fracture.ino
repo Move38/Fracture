e3fabca7037f6898c61a2ee26f6d60310fbcf32e
Jonathan Bobrow <jbobrow@gmail.com>
2018-08-23 23:20:03 -0400

    fracture recognition from dan and team

--- Fracture.ino
+++ Fracture.ino
@@ -1,105 +1,135 @@
-/*  Fracture (Game)
- *  
- *  Setup: For 3-6 players. Each player gets 5-6 tiles of a specific color.
- *         Game starts will all tiles connected, but completely segregated by color.
- *
- *  Goal: Get your tiles to be touching only other players tiles.
- *        Your tiles must be touching at least two other tiles to feel happily diverse.
- *
- *  Turns: Start from a player of choice and then go clockwise taking turns.
- *         A turn consists of fracturing the population into two sub populations,
- *         and then connecting them back together in any way you please.
- *  
- *  When a player has a tile fully diversified, it will blink in unison with other blinking Blinks of its color,
- *  first player to have all of their tiles blink wins. 
- *
- *  Game development by: Celia Pearce, Jeanie Choi, Isabella Carlson, Mike Lazer-Walker, Joshua Sloane
- *
- *  by Jonathan Bobrow
- *  9.28.2017
- */
-
-#define HAPPY_FLASH_DURATION 500
-
-Color displayColor;
-
-Color teamColors[] = {RED, BLUE, YELLOW, GREEN};
-
-byte teamIndex = 0;
-
-Timer happyFlashTimer;
-bool happyFlashOn;
-
+byte gameState;
+byte team;
+byte teamHues[] = {22, 49, 82, 99};
+enum fractureStates {NOMINAL, FRACTURED, RESOLVING};
+bool neighborStates[6];
+//animation variables
+Timer animTimer;
+byte saturation = 255;
+byte brightness = 255;
+Timer happyTimer;
+bool flashOn = true;
 void setup() {
-}
-
-void loop() {
-  
-  // change team if triple clicked
-  if (buttonMultiClicked()) {
-    if (buttonClickCount() == 3) {
-      teamIndex++;
-      if(teamIndex>= COUNT_OF(teamColors)) {
-        teamIndex = 0;      
-      }
-    }
-  }
-
-  byte numNeighbors = 0;
-  bool noNeighborsOfSameColor = true;
-
-  // look at neighbors
+  gameState = NOMINAL;
+  setColor(makeColorHSB(teamHues[team], 255, 255));
   FOREACH_FACE(f) {
-    if(!isValueReceivedOnFaceExpired(f)) {
-      
+    if (!isValueReceivedOnFaceExpired(f)) {
+      neighborStates[f] = true;
+    } else {
+      neighborStates[f] = false;
+    }
+  }
+}
+void loop() {
+  //first, we need to listen for double clicks to change team
+  if (buttonDoubleClicked()) {
+    team++;
+    if (team > 3) {
+      team = 0;
+    }
+    setColor(makeColorHSB(teamHues[team], 255, 255));
+  }
+  //determine which state I'm in, send to appropriate loop behavior
+  switch (gameState) {
+    case NOMINAL:
+      nominalLoop();
+      break;
+    case FRACTURED:
+      fracturedLoop();
+      break;
+    case RESOLVING:
+      resolvingLoop();
+      break;
+  }
+  //this is where you update your face list
+  FOREACH_FACE(f) {
+    if (!isValueReceivedOnFaceExpired(f)) {
+      neighborStates[f] = true;
+    } else {
+      neighborStates[f] = false;
+    }
+  }
+  //last, we want to resolve any flashes that have been initiated
+  if (animTimer.isExpired() && (saturation != 255 || brightness != 255)) {
+    if (saturation != 255) {
+      saturation += 17;
+    }
+    if (brightness != 255) {
+      brightness += 17;
+    }
+    animTimer.set(50);
+    setColor(makeColorHSB(teamHues[team], saturation, brightness));
+  }
+  if (happyTimer.isExpired()) {
+    if (saturation == 255 && brightness == 255) { //this only happens if no other flashes are happening
+      if (happinessCheck()) {
+        if (flashOn) {
+          setColor(OFF);
+          flashOn = false;
+        } else {
+          setColor(makeColorHSB(teamHues[team], 255, 255));
+          flashOn = true;
+        }
+      }
+    }
+    happyTimer.set(500);
+  }
+  setValueSentOnAllFaces((gameState * 10) + team);
+}
+void nominalLoop() {
+  //check surroundings for MISSING NEIGHBORS or neighbors already in distress
+  FOREACH_FACE(f) {
+    if (isValueReceivedOnFaceExpired(f) && neighborStates[f] == true) { //missing neighbor
+      gameState = FRACTURED;
+    } else if (!isValueReceivedOnFaceExpired(f) && getLastValueReceivedOnFace(f) / 10 == FRACTURED) { //distressed neighbor
+      gameState = FRACTURED;
+    }
+  }
+  if (gameState == FRACTURED) {
+    //begin the off flash, change the state
+    setColor(OFF);
+    brightness = 0;
+  }
+}
+void fracturedLoop() {
+  //first we do the flash animation
+  FOREACH_FACE(f) {
+    if (!isValueReceivedOnFaceExpired(f) && neighborStates[f] == false) { //new neighbor
+      gameState = RESOLVING;
+    } else if (!isValueReceivedOnFaceExpired(f) && getLastValueReceivedOnFace(f) / 10 == RESOLVING) { //resolving neighbor
+      gameState = RESOLVING;
+    }
+  }
+  if (gameState == RESOLVING) {
+    setColor(WHITE);
+    saturation = 0;
+  }
+}
+void resolvingLoop() {
+  gameState = NOMINAL;
+  FOREACH_FACE(f) {
+    if (!isValueReceivedOnFaceExpired(f) && getLastValueReceivedOnFace(f) / 10 == FRACTURED) {
+      gameState = RESOLVING;
+    }
+  }
+  if (gameState == NOMINAL) {
+    setColor(makeColorHSB(teamHues[team], 255, 255));
+  }
+}
+bool happinessCheck() {
+  bool isHappy = true;
+  byte numNeighbors = 0;
+  FOREACH_FACE(f) {
+    byte neighbor = getLastValueReceivedOnFace(f) % 10;
+    if (!isValueReceivedOnFaceExpired(f)) { //this means there is something there
       numNeighbors++;
-    
-      // if their color is the same as mine... not happy
-      if (getLastValueReceivedOnFace(f) == teamIndex) {
-        noNeighborsOfSameColor = false;
+      if (neighbor == team) { //this means this neighbor is the same color as us. Oh no!
+        isHappy = false;
       }
     }
   }
-
-  bool isHappy = false;
-  
-  // if I have two neighbors or more and my neighbors are not my color i'm happy
-  if (numNeighbors >= 2 && noNeighborsOfSameColor) {
-    isHappy = true;
+  if (numNeighbors < 2) { //if numNeighbors never reached 2, it's not happy
+    isHappy = false;
   }
-
-  bool isOn = true;
-  
-  // if I'm happy
-  if (isHappy) {
-
-    if(happyFlashTimer.isExpired()) {
-      happyFlashOn = !happyFlashOn;
-      
-      happyFlashTimer.set(HAPPY_FLASH_DURATION);
-    }
-    
-    if(!happyFlashOn) {
-      isOn = false;
-    }
-  }
-
-  if(isOn) {
-    // have the color on the Blink raise and lower to feel more alive
-    byte bri = 220 + 35 * sin_d( (millis()/10) % 360); // oscillate between values 185 and 255
-    setColor(dim(teamColors[teamIndex], bri));
-  }
-  else {
-    setColor(OFF);
-  }
-
-  setValueSentOnAllFaces(teamIndex);
-}
-
-
-// Sin in degrees ( standard sin() takes radians )
-
-float sin_d( uint16_t degrees ) {
-
-    return sin( ( degrees / 360.0F ) * 2.0F * PI   );
+  return isHappy;
 }
