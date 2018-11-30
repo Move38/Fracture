9cce220aa8fed17fd943d185a1837b548e37d880
Jonathan Bobrow <jbobrow@gmail.com>
2018-11-30 00:47:53 -0500

    Update Fracture.ino
    highlight edge on connect and dim edge on disconnect working well

--- Fracture.ino
+++ Fracture.ino
@@ -1,25 +1,26 @@
 /*  Fracture (Game)
- *  
- *  Setup: For 3-6 players. Each player gets 5-6 tiles of a specific color.
- *         Game starts will all tiles connected, but completely segregated by color.
- *
- *  Goal: Get your tiles to be touching only other players tiles.
- *        Your tiles must be touching at least two other tiles to feel happily diverse.
- *
- *  Turns: Start from a player of choice and then go clockwise taking turns.
- *         A turn consists of fracturing the population into two sub populations,
- *         and then connecting them back together in any way you please.
- *  
- *  When a player has a tile fully diversified, it will blink in unison with other blinking Blinks of its color,
- *  first player to have all of their tiles blink wins. 
- *
- *  Game development by: Celia Pearce, Jeanie Choi, Isabella Carlson, Mike Lazer-Walker, Joshua Sloane
- *
- *  by Jonathan Bobrow
- *  9.28.2017
- */
+
+    Setup: For 3-6 players. Each player gets 5-6 tiles of a specific color.
+           Game starts will all tiles connected, but completely segregated by color.
+
+    Goal: Get your tiles to be touching only other players tiles.
+          Your tiles must be touching at least two other tiles to feel happily diverse.
+
+    Turns: Start from a player of choice and then go clockwise taking turns.
+           A turn consists of fracturing the population into two sub populations,
+           and then connecting them back together in any way you please.
+
+    When a player has a tile fully diversified, it will blink in unison with other blinking Blinks of its color,
+    first player to have all of their tiles blink wins.
+
+    Game development by: Celia Pearce, Jeanie Choi, Isabella Carlson, Mike Lazer-Walker, Joshua Sloane
+
+    by Jonathan Bobrow
+    9.28.2017
+*/
 
 #define HAPPY_FLASH_DURATION 500
+#define EDGE_FADE_DURAION    500
 
 Color displayColor;
 
@@ -31,17 +32,22 @@ byte teamIndex = 0;
 Timer happyFlashTimer;
 bool happyFlashOn;
 
+Timer edgeTimer[6];
+bool  edgeAcquired;
+
+bool hasRecentlySeenNeighbor[6];
+
 void setup() {
 }
 
 void loop() {
-  
+
   // change team if triple clicked
   if (buttonMultiClicked()) {
     if (buttonClickCount() == 3) {
       teamIndex++;
-      if(teamIndex>= COUNT_OF(teamHues)) {
-        teamIndex = 0;      
+      if (teamIndex >= COUNT_OF(teamHues)) {
+        teamIndex = 0;
       }
     }
   }
@@ -51,43 +57,56 @@ void loop() {
 
   // look at neighbors
   FOREACH_FACE(f) {
-    if(!isValueReceivedOnFaceExpired(f)) {
-      
+    if (!isValueReceivedOnFaceExpired(f)) {
+
+      if (hasRecentlySeenNeighbor[f] == false) {
+        edgeAcquired = true;
+        edgeTimer[f].set(EDGE_FADE_DURAION);
+      }
+      hasRecentlySeenNeighbor[f] = true;
+
       numNeighbors++;
-    
+
       // if their color is the same as mine... not happy
       if (getLastValueReceivedOnFace(f) == teamIndex) {
         noNeighborsOfSameColor = false;
       }
     }
+    else {
+      if (hasRecentlySeenNeighbor[f] == true) {
+        edgeAcquired = false;
+        edgeTimer[f].set(EDGE_FADE_DURAION);
+      }
+      hasRecentlySeenNeighbor[f] = false;
+    }
   }
 
   bool isHappy = false;
-  
+
   // if I have two neighbors or more and my neighbors are not my color i'm happy
   if (numNeighbors >= 2 && noNeighborsOfSameColor) {
     isHappy = true;
   }
 
   bool isOn = true;
-  
+
   // if I'm happy
   if (isHappy) {
 
-    if(happyFlashTimer.isExpired()) {
+    if (happyFlashTimer.isExpired()) {
       happyFlashOn = !happyFlashOn;
-      
+
       happyFlashTimer.set(HAPPY_FLASH_DURATION);
     }
-    
-    if(!happyFlashOn) {
+
+    if (!happyFlashOn) {
       isOn = false;
     }
   }
 
-  if(isOn) {
+  if (isOn) {
     // have the color on the Blink raise and lower to feel more alive
-    byte bri = 220 + 35 * sin_d( (millis()/10) % 360); // oscillate between values 185 and 255
+    byte bri = 220 + 35 * sin_d( (millis() / 10) % 360); // oscillate between values 185 and 255
     setColor(dim(getColorForTeam(teamIndex), bri));
   }
   else {
@@ -95,11 +114,24 @@ void loop() {
   }
 
   // display fracture animation or mend animation
-  // if we just lost a neighbor fade up from dark
-  // if we just gained a neighbor saturate from white
+  FOREACH_FACE(f) {
+    if (!edgeTimer[f].isExpired()) {
+      if (edgeAcquired) {
+        // if we just gained a neighbor saturate from white
+        byte sat = 255 - (255 * edgeTimer[f].getRemaining() )/ EDGE_FADE_DURAION;
+        setColorOnFace(makeColorHSB(teamHues[teamIndex], sat, 255), f);
+      }
+      else {
+        // if we just lost a neighbor fade up from dark
+        byte bri = 255 - (255 * edgeTimer[f].getRemaining() )/ EDGE_FADE_DURAION;
+        setColorOnFace(makeColorHSB(teamHues[teamIndex], 255, bri), f);
+      }
+    }
+  }
 
   setValueSentOnAllFaces(teamIndex);
 }
+
 
 Color getColorForTeam(byte t) {
   return makeColorHSB(teamHues[t], 255, 255);
@@ -110,5 +142,5 @@ Color getColorForTeam(byte t) {
 
 float sin_d( uint16_t degrees ) {
 
-    return sin( ( degrees / 360.0F ) * 2.0F * PI   );
+  return sin( ( degrees / 360.0F ) * 2.0F * PI   );
 }
